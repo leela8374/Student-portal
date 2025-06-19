b38d1c4005078541fcec4abd9f2a01d234ec2a9c
leela8374 <leelamohankurmapu2004@gmail.com>
2025-06-19 10:57:03 +0530

    Update app.js

--- almostdone-main/backned/app.js
+++ almostdone-main/backned/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000'],  // Frontend URL
+  origin: ['https://student-portal-dygx.vercel.app/'],  // Frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
